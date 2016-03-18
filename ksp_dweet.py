65a2e58383366fe6598dbdfa5980ec97d2167a67
likeitneverwentaway <apulido@free.fr>
2016-03-18 01:35:13 +0100

    update

--- ksp_dweet.py
+++ ksp_dweet.py
@@ -12,9 +12,6 @@ vessel.subscribe("resource_ec_current")
 vessel.subscribe("resource_ec_max")
 vessel.subscribe("vessel_body")
 
-
-
-
 def truncate(n):
     try :
         n = round(n,1)
@@ -29,32 +26,33 @@ def timec(n):
     except:
         h,m,s = 0,0,0
     return "%d:%02d:%02d" % (h, m, s)
+    
 vessel.start()
 thingId = "kspksp_dweet_test"  
 while 1:
     time.sleep(0.5)
     myDweet = {}
     
-    
     vessel_altitude = truncate(vessel.vessel_altitude)
-    myDweet['Vessel Altitude'] = vessel_altitude
-    
     vessel_periapsis = truncate(vessel.vessel_periapsis)
-    myDweet['Vessel Periapsis'] = vessel_periapsis
-    
+    vessel_apoapsis = truncate(vessel.vessel_apoapsis)
     vessel_time_to_periapsis = timec(vessel.vessel_time_to_periapsis)
-    myDweet['Time To Periapsis'] = vessel_time_to_periapsis
-    
     vessel_time_to_apoapsis = timec(vessel.vessel_time_to_apoapsis)
-    myDweet['Time To Apoapsis'] = vessel_time_to_apoapsis
+    vessel_mission_time = timec(vessel.vessel_mission_time)
     
-    myDweet['EletricCharge Current'] = truncate(vessel.resource_ec_current)
-    myDweet['EletricCharge Max'] = truncate(vessel.resource_ec_max)
+    myDweet = {
+               'Mission Time' : vessel_mission_time,
+               'Vessel Body' : vessel.vessel_body,
+               'Vessel Altitude' : vessel_altitude,
+               'Vessel Periapsis' : vessel_periapsis,
+               'Vessel Apoapsis' : vessel_apoapsis,
+               'Time To Periapsis' : vessel_time_to_periapsis,
+               'Time To Apoapsis' : vessel_time_to_apoapsis,
+               'EletricCharge Current' : truncate(vessel.resource_ec_current),
+               'EletricCharge Max' : truncate(vessel.resource_ec_max),
+               }
     
-    myDweet['Vessel Body'] = vessel.vessel_body
-
+    
     
     dweepy.dweet_for(thingId,myDweet)
     print 'Still working alright.'
-    # print vessel.vessel_altitude
-    # print vessel.vessel_mission_time
